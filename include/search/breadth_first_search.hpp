f5bcdef76ccf0af56b54f143c3377b30ee173fb5
James Gadoury <gadouryjames@gmail.com>
2023-07-15 00:03:01 -0400

    More fun stuff

---
+++ include/search/breadth_first_search.hpp
@@ -0,0 +1,13 @@
+#include "search/best_first_search.hpp"
+
+namespace search {
+
+template<IsProblem Problem>
+std::optional<std::shared_ptr<typename Problem::Node>> breadth_first_search(const std::shared_ptr<Problem> problem) {
+    return best_first_search(
+        problem,
+        [](std::shared_ptr<typename Problem::Node> a, std::shared_ptr<typename Problem::Node> b) {return a->path_cost < b->path_cost; }
+    );
+}
+
+}
--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
 #include <search/map_problem.hpp>
-#include <search/best_first_search.hpp>
+#include <search/depth_first_search.hpp>
+#include <search/breadth_first_search.hpp>
 
 #include <iostream>
 
 using namespace std;
 using namespace search;
 
-int main(int argc, char *argv[]) {
+void execute_depth_first_search() {
     const shared_ptr<MapProblem::ProblemType> problem = make_shared<MapProblem>(MapProblemConfig{.rows=9,.cols=9,.initial=MapEntry{.row=7,.col=2},.goal=MapEntry{.row=0,.col=5}});
 
-    auto cmp = [](shared_ptr<MapProblem::Node> a, shared_ptr<MapProblem::Node> b) {return a->path_cost < b->path_cost; };
-    auto found_solution = best_first_search(problem, cmp);
+    auto found_solution = depth_first_search(problem);
 
     if (!found_solution.has_value()) { 
         cout << "Failed to find solution." << endl;
-        return -1;
+        return;
     } 
 
     cout << "Found state: " << found_solution.value()->state << endl;
     cout << "Cost: " << found_solution.value()->path_cost << endl;
+}
 
+void execute_breadth_first_search() {
+    const shared_ptr<MapProblem::ProblemType> problem = make_shared<MapProblem>(MapProblemConfig{.rows=9,.cols=9,.initial=MapEntry{.row=7,.col=2},.goal=MapEntry{.row=0,.col=5}});
+
+    auto found_solution = breadth_first_search(problem);
+
+    if (!found_solution.has_value()) { 
+        cout << "Failed to find solution." << endl;
+        return;
+    } 
+
+    cout << "Found state: " << found_solution.value()->state << endl;
+    cout << "Cost: " << found_solution.value()->path_cost << endl;
+}
+
+int main(int argc, char *argv[]) {
+    cout << "Executing depth first search..." << endl;
+    execute_depth_first_search();
+
+    cout << "Executing breadth first search..." << endl;
+    execute_breadth_first_search();
 }
